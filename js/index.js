8ebdccf2bac0b32ddfaff8de198874963405df65
Brenda G.O. <172094075+bgarciaort@users.noreply.github.com>
2025-10-22 00:36:39 -0500

    finish api project

--- js/index.js
+++ js/index.js
@@ -1,26 +1,48 @@
-fetch('https://api.github.com/users/bgarciaort/repos')  
-   .then(response => {
-    if (!response.ok) {
-      throw new Error('Request failed');
-    }
-    return response.json(); 
-  })
-  .then(data => {
-    const repositories = data;
-    console.log(repositories); 
-    const projectSection = document.getElementById('myProjects'); 
-    const projectList = projectSection.querySelector('ul');
+  /** Cat API */    
+    
+    const API_KEY = "live_SoDfrxb9DUlzHsVFF9SFTIFVGwbgOR5mkOFV21ojwjOrV1Ah8GLCr56RVtgWLIhf"; 
+    const CAT_API_URL = "https://api.thecatapi.com/v1/images/search?limit=1";
 
-    for (let i = 0; i < repositories.length; i++) {
-      const project = document.createElement('li'); 
-      project.innerText = repositories[i].name; 
-      projectList.appendChild(project); 
-    }
-  })
-  .catch(error => {
-    console.error('An error occurred:', error);
-    const projectSection = document.getElementById('myProjects');
-    const errorMessage = document.createElement('p');
-    errorMessage.innerText = 'Could not load projects. Please try again later.';
-    projectSection.appendChild(errorMessage); 
-  });
+    function fetchCatImage(){
+    fetch(CAT_API_URL, {
+        headers: {
+            "x-api-key": API_KEY 
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed');
+        }
+        return response.json(); 
+      })
+
+    .then(data => {
+        const imgElement = document.createElement('img');
+        imgElement.src = data[0].url;
+        const kittyContainer = document.getElementById('cat-container');
+        kittyContainer.innerHTML = ''; 
+        kittyContainer.appendChild(imgElement);
+    })
+
+    .catch(error => {
+        console.error("An error occurred fetching your cat image:", error);
+    });
+
+  }
+fetchCatImage();
+
+/** Button */
+
+document.getElementById('cat-button').addEventListener('click', fetchCatImage);
+
+/** Footer */
+
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+copyright.innerHTML = `&copy; Brenda García Ortega ${thisYear}`;
+copyright.style.fontSize = "15px" 
+copyright.style.textAlign = 'right';
+copyright.style.marginRight = '20px';
+footer.appendChild(copyright);
